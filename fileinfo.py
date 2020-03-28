65e60517ae720321d4815cd60deb4c29cdcb2f1a
Hans-2019 <henryh156@yahoo.com>
2020-03-28 21:57:06 +0800

    update fileinfo.py 2

--- fileinfo.py
+++ fileinfo.py
@@ -10,3 +10,12 @@ import tkinter.simpledialog as sim
 import matplotlib.pyplot as plt
 plt.rcParams['font.sans-serif']=['Simhei']
 import webbrowser
+
+root=Tk()
+root.geometry('750x560')
+root.title('文本编辑器')
+mes.showinfo('Welcome','欢迎使用文本编辑器!')
+Label(root,text='欢迎使用文本编辑器',fg='blue',font=50).place(x=300,y=30)
+Label(root,text='文本输入',fg='green',font=30).place(x=10,y=80)
+text1=Text(root,bd=3,fg='red')
+text1.place(x=110,y=80)
