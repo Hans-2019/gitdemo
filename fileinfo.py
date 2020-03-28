5bd71836648489219a3bb629d7f7c31be2634aae
Hans-2019 <henryh156@yahoo.com>
2020-03-28 21:49:06 +0800

    update fileinfo.py 1

--- fileinfo.py
+++ fileinfo.py
@@ -4,3 +4,9 @@ Created on Sun Jun  9 12:25:15 2019
 
 @author: qiansihan
 """
+from tkinter import *
+import tkinter.messagebox as mes
+import tkinter.simpledialog as sim
+import matplotlib.pyplot as plt
+plt.rcParams['font.sans-serif']=['Simhei']
+import webbrowser
